7ce60b1b72821042cda2dc7a3e7c1ec4220b6073
BuildTools <unconfigured@null.spigotmc.org>
2024-12-15 20:55:52 +0200

    - Updated plugin dependencies. - Removed redundant Vault refferences.

--- src/main/java/me/orphey/areyoutypingplugin/AreYouTypingPlugin.java
+++ src/main/java/me/orphey/areyoutypingplugin/AreYouTypingPlugin.java
@@ -18,13 +18,7 @@ public final class AreYouTypingPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // VAULT INIT -------------------------------------------------
-//        if (!setupEconomy() ) {
-//            getLogger().severe("Disabled due to no Vault dependency found!");
-//            getServer().getPluginManager().disablePlugin(this);
-//            return;
-//        }
         setupPermissions();
-        //setupChat();
         // END VAULT INIT
         // PACKET EVENTS
         PacketEvents.getAPI().getEventManager().registerListener(
@@ -48,32 +42,8 @@ public final class AreYouTypingPlugin extends JavaPlugin {
     }
 
     // VAULT INITIALIZATION -----------------------------------------------------------------------------------
-    private static Economy econ = null;
     @SuppressWarnings("FieldCanBeLocal")
     private static Permission perms = null;
-    @SuppressWarnings("FieldCanBeLocal")
-    private static Chat chat = null;
-    @SuppressWarnings("ConstantValue")
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-    public static Economy getEconomy() {
-        return econ;
-    }
-    @SuppressWarnings({"unused", "DataFlowIssue", "ConstantValue"})
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
-        return chat != null;
-    }
     @SuppressWarnings({"DataFlowIssue", "ConstantValue", "UnusedReturnValue"})
     private boolean setupPermissions() {
         RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
