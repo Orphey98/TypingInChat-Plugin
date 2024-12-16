322c849d3f642971bbcf308e3329527fa9db37a8
Orphey98 <stalker664@gmail.com>
2024-12-16 23:54:36 +0200

    - Boolean parameters validation in ConfigLoader

--- src/main/java/me/orphey/areyoutypingplugin/ConfigLoader.java
+++ src/main/java/me/orphey/areyoutypingplugin/ConfigLoader.java
@@ -5,27 +5,34 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.util.Vector;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.Map.entry;
 
 public class ConfigLoader {
     private ConfigLoader() {
     }
     private static final ConfigLoader instance = new ConfigLoader();
-    private static YamlConfiguration config = new YamlConfiguration();
+    private static final YamlConfiguration config = new YamlConfiguration();
+    private static final Map<String, Boolean> booleanParameters = new HashMap<>(Map.ofEntries(
+            entry("show-names", true),
+            entry("icon-indentation", true),
+            entry("visible-through-blocks", false),
+            entry("text-shadow", false)
+    ));
     private static List<Double> location;
     private static List<Float> translation;
-    private static boolean showNames;
-    private static boolean isIndentation;
     private static int viewRange;
-    private static boolean visibleThroughBlocks;
     private static String typingChar;
     private static String backgroundColor;
     private static int backgroundTransparency;
     private static String namesColor;
     private static String typingIconColor;
-    private static boolean textShadow;
 
     public static Vector getLocation() {
         double x = location.get(0);
@@ -40,24 +47,23 @@ public class ConfigLoader {
         return new Vector(x, y, z);
     }
     public static boolean isShowNames() {
-        return showNames;
+        return booleanParameters.get("show-names");
     }
     public static boolean isIndentation() {
-        return isIndentation;
+        return booleanParameters.get("icon-indentation");
     }
     public static int getViewRange() {
         return viewRange;
     }
     public static boolean isVisibleThroughBlocks() {
-        return visibleThroughBlocks;
+        return booleanParameters.get("visible-through-blocks");
     }
     public static String getTypingChar() {
         return typingChar;
     }
     public static boolean isTextShadow() {
-        return textShadow;
+        return booleanParameters.get("text-shadow");
     }
-
     public static String getNamesColor() {
         return namesColor;
     }
@@ -76,6 +82,7 @@ public class ConfigLoader {
         config.load(file);
         loadOptions();
     }
+
     // TODO validate config params
     // Data types:
     // boolean
@@ -83,21 +90,30 @@ public class ConfigLoader {
     // typing-char: string, max length 16 chars
     // hex color
     // background-transparency: int from 0 to 255
+
+    private static void validateBoolean() {
+        for (String parameter : booleanParameters.keySet()) {
+            if (config.isBoolean(parameter)) {
+                booleanParameters.put(parameter, config.getBoolean(parameter));
+            } else {
+                logger.warning(String.format("Can't load %s parameter from config. Using default value.", parameter));
+            }
+        }
+    }
+
     private static void loadOptions() {
+        validateBoolean();
         location = config.getDoubleList("location");
         translation = config.getFloatList("transformation");
-        showNames = config.getBoolean("show-names");
-        isIndentation = config.getBoolean("icon-indentation");
-        viewRange = config.getInt("view-range");
-        visibleThroughBlocks = config.getBoolean("visible-through-blocks");
+
         typingChar = config.getString("typing-char");
-        textShadow = config.getBoolean("text-shadow");
         namesColor = config.getString("names-color");
         typingIconColor = config.getString("typing-icon-color");
         backgroundColor = config.getString("background-color");
         backgroundTransparency = config.getInt("background-transparency");
     }
 
+    private static final Logger logger = AreYouTypingPlugin.getInstance().getPluginLogger();
     public static ConfigLoader getInstance() {
         return instance;
     }
