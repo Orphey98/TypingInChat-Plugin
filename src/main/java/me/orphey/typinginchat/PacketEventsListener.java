e8e2dbb1bce500931e0b5710fe14f8e2549bfb48
Orphey98 <stalker664@gmail.com>
2025-01-15 21:24:32 +0200

    - Forge packets logic

--- src/main/java/me/orphey/typinginchat/PacketEventsListener.java
+++ src/main/java/me/orphey/typinginchat/PacketEventsListener.java
@@ -30,7 +30,7 @@ public class PacketEventsListener implements PacketListener {
         if (event.getPacketType() == PacketType.Play.Client.PLUGIN_MESSAGE) {
             WrapperPlayClientPluginMessage packet = new WrapperPlayClientPluginMessage(event);
             String channel = packet.getChannelName(); // Get the channel name
-
+            //System.out.println("Received packet " + channel);
             if (channel.equals("typinginchatmod:typing_status")) {
                 return packet;
             } else {
@@ -43,12 +43,15 @@ public class PacketEventsListener implements PacketListener {
 
     private byte readPacket(WrapperPlayClientPluginMessage packet) {
         byte[] data = packet.getData();
-
+        //System.out.println("Data Length: " + data.length);
         if (data.length == 2) { //Forge
+            //System.out.println("Forge " + data[1]);
             return data[1];
         } else if (data.length == 1) { //Fabric
+            //System.out.println("Fabric " + data[0]);
             return data[0];
         } else {
+            //System.out.println("Empty");
             return 0;
         }
 
